42c57f512aac06d4b667dacdf2506e3d8e5575c2
Christoffer Cajander <christoffer.bjorn.cajander@gmail.com>
2020-05-24 14:28:08 +0200

    Added ability to pick custom web addres to menu

--- middleware/users.js
+++ middleware/users.js
@@ -1,18 +1,23 @@
     // middleware/users.js
     module.exports = {
         validateRegister: (req, res, next) => {
+
+          const forbiddenNames = ['home', 'sign-up', 'login', 'meny', 'about', 'account', 'admin']
+
           // username min length 3
           if (!req.body.username || req.body.username.length < 3) {
             return res.status(400).send({
               msg: 'Ditt användarnamn måste bestå av minst tre tecken.'
             });
           }
+
           // password min 6 chars
           if (!req.body.password || req.body.password.length < 6) {
             return res.status(400).send({
               msg: 'Ditt lösenord får inte vara kortare än sex tecken.'
             });
           }
+
           // password (repeat) does not match
           if (
             !req.body.password_repeat ||
@@ -22,6 +27,14 @@
               msg: 'Lösenorden matchar inte.'
             });
           }
+
+           // forbidden route names
+           if(forbiddenNames.includes(req.body.routeName)) {
+              return res.status(400).send({
+                msg: 'Webbadressen innehåller ett förbjudet ord, prova igen.'
+              });
+           }
+
           next();
         },
         isLoggedIn: (req, res, next) => {
